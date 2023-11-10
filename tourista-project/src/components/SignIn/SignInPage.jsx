bec0d0096e8547da7336b78efbb40a83b0348a6c
EkberAkh <ekber.0303@gmail.com>
2023-11-10 23:07:35 +0400

    add prev arrow

--- tourista-project/src/components/SignIn/SignInPage.jsx
+++ tourista-project/src/components/SignIn/SignInPage.jsx
@@ -1,18 +1,20 @@
 import React from "react";
 import "./SignInPage.css";
 import signInImg from "../../assets/images/signin.png";
-import { Link, Route, Routes, useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import logo from "../../assets/images/Logo_tour.png";
+import prevArr from "../../assets/images/chevron-left.png";
 
-import InputBox from "./InputBox";
-import SocialMedia from "./SocialMedia";
-import SignInDown from "./SignInDown";
 import LoginPage from "./LoginPage";
 import ForgotPass from "./ForgotPass";
 import CreateAcc from "./CreateAcc";
 
 const SignInPage = () => {
-const location = useLocation()
+  const location = useLocation();
+
+  const prevClick = () => {
+    window.history.back();
+  };
 
   return (
     <div className="sign-in">
@@ -22,18 +24,24 @@ const location = useLocation()
         <div className="container">
           <div className="sign-in-right-inner">
             <div className="right-head">
+              {location.pathname === "/forgot-password" && (
+                <div onClick={prevClick} className="prev-arrow">
+                  <img src={prevArr} alt="prevArr" />
+                </div>
+              )}
+              {location.pathname === "/create-acc" && (
+                <div onClick={prevClick} className="prev-arrow">
+                  <img src={prevArr} alt="prevArr" />
+                </div>
+              )}
               <Link to="/">
                 <img src={logo} alt="logo" />
               </Link>
             </div>
-            {location.pathname === '/sign-in' && <LoginPage/>}
-            {location.pathname === '/forgot-password' && <ForgotPass/>}
-            {location.pathname === '/create-acc' && <CreateAcc/>}
-          
-        
 
-            
-           
+            {location.pathname === "/sign-in" && <LoginPage />}
+            {location.pathname === "/forgot-password" && <ForgotPass />}
+            {location.pathname === "/create-acc" && <CreateAcc />}
           </div>
         </div>
       </div>
